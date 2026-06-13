21bfc030b5fe1ae77c7123ecf8a4704d369fb32e
phuchoang1807 <BIT230323@st.cmc-u.edu.vn>
2026-06-13 14:54:39 +0700

    feat: implement payment integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '500e-2001-ee0-1ac2-4285-d904-e8e-8a44-4928.ngrok-free.app'
+    ]
+  }
 })
