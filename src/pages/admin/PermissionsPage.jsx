991c6bcffb3b699662a9e61d2ac9225d4b460896
Anh-star <anhdh205@gmail.com>
2026-08-26 04:02:42 +0700

    fix(admin): import useMemo in PermissionsPage

--- src/pages/admin/PermissionsPage.jsx
+++ src/pages/admin/PermissionsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import AdminPageHeader from '../../components/admin/AdminPageHeader';
 import AdminTableWrapper from '../../components/admin/AdminTableWrapper';
